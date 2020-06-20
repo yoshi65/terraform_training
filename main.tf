440af4161b42e90b57c15e41fb2362d91a72311e
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2020-06-20 19:47:29 +0900

    updated

--- main.tf
+++ main.tf
@@ -16,6 +16,39 @@ module "desccribe_regions_for_ecs" {
   policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
+module "example_sg" {
+  source      = "./security_group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 /* output "public_dns" {
   value = module.web_server.public_dns
+} */
+
+resource "aws_vpc" "example" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "example"
+  }
+}
+
+/* resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
 } */
