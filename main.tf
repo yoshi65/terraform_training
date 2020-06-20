d730956ba93dcfacf954b7dddce54eb6e9bd3637
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2020-06-20 09:41:07 +0900

    added auth

--- main.tf
+++ main.tf
@@ -4,11 +4,18 @@ provider "aws" {
   profile                 = "terraform"
 }
 
-module "web_server" {
+/* module "web_server" {
   source        = "./http_server"
   instance_type = "t3.micro"
+} */
+
+module "desccribe_regions_for_ecs" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
-output "public_dns" {
+/* output "public_dns" {
   value = module.web_server.public_dns
-}
+} */
